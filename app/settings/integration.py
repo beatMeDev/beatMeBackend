37f3943cae79a711d58fdc6d480336501093bb1b
Ilia Snezhkov <iseption@gmail.com>
2020-07-24 15:29:31 +0300

    HOTFIX: default value in settings for running pytest without .env

--- app/settings/integration.py
+++ app/settings/integration.py
@@ -1,22 +1,22 @@
 from app.settings.base import config
 
 
-FACEBOOK_ID: str = config("FACEBOOK_ID", cast=str)
-FACEBOOK_SECRET: str = config("FACEBOOK_SECRET", cast=str)
-FACEBOOK_REDIRECT_URI: str = config("FACEBOOK_REDIRECT_URI", cast=str)
-FACEBOOK_API_VERSION: str = config("FACEBOOK_API_VERSION", cast=str)
+FACEBOOK_ID: str = config("FACEBOOK_ID", cast=str, default="")
+FACEBOOK_SECRET: str = config("FACEBOOK_SECRET", cast=str, default="")
+FACEBOOK_REDIRECT_URI: str = config("FACEBOOK_REDIRECT_URI", cast=str, default="")
+FACEBOOK_API_VERSION: str = config("FACEBOOK_API_VERSION", cast=str, default="")
 
-GOOGLE_ID: str = config("GOOGLE_ID", cast=str)
-GOOGLE_SECRET: str = config("GOOGLE_SECRET", cast=str)
-GOOGLE_REDIRECT_URI: str = config("GOOGLE_REDIRECT_URI", cast=str)
+GOOGLE_ID: str = config("GOOGLE_ID", cast=str, default="")
+GOOGLE_SECRET: str = config("GOOGLE_SECRET", cast=str, default="")
+GOOGLE_REDIRECT_URI: str = config("GOOGLE_REDIRECT_URI", cast=str, default="")
 
-SPOTIFY_ID: str = config("SPOTIFY_ID", cast=str)
-SPOTIFY_SECRET: str = config("SPOTIFY_SECRET", cast=str)
-SPOTIFY_REDIRECT_URI: str = config("SPOTIFY_REDIRECT_URI", cast=str)
+SPOTIFY_ID: str = config("SPOTIFY_ID", cast=str, default="")
+SPOTIFY_SECRET: str = config("SPOTIFY_SECRET", cast=str, default="")
+SPOTIFY_REDIRECT_URI: str = config("SPOTIFY_REDIRECT_URI", cast=str, default="")
 
-VK_ID: int = config("VK_ID", cast=int)
-VK_SECRET: str = config("VK_SECRET", cast=str)
-VK_REDIRECT_URI: str = config("VK_REDIRECT_URI", cast=str)
-VK_API_VERSION: str = config("VK_API_VERSION", cast=str)
+VK_ID: int = config("VK_ID", cast=int, default=0)
+VK_SECRET: str = config("VK_SECRET", cast=str, default="")
+VK_REDIRECT_URI: str = config("VK_REDIRECT_URI", cast=str, default="")
+VK_API_VERSION: str = config("VK_API_VERSION", cast=str, default="")
 
-YOUTUBE_KEY: str = config("YOUTUBE_KEY", cast=str)
+YOUTUBE_KEY: str = config("YOUTUBE_KEY", cast=str, default="")
