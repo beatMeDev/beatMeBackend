1585d72364fb1b18c19cf0723155dac9eb50bcbe
Ilia Snezhkov <iseption@gmail.com>
2020-07-29 12:44:22 +0300

    sprint-1: add track details route

--- app/routes/tracks.py
+++ app/routes/tracks.py
@@ -1,6 +1,7 @@
 """Tracks endpoints"""
 from random import randint
 from typing import Optional
+from uuid import UUID
 
 from fastapi import APIRouter
 from tortoise.contrib.pydantic import PydanticModel
@@ -21,6 +22,19 @@ async def get_random_track_route() -> PydanticModel:
     count: int = await Track.filter(recommended=True).count()
     offset: int = randint(0, count - 1)
     track: Optional[Track] = await Track.filter(recommended=True).limit(1).offset(offset).first()
+    response: PydanticModel = await TrackOut.from_tortoise_orm(track)
+
+    return response
+
+
+@tracks_router.get("/{track_id}/", response_model=TrackOut, summary="Return track")
+async def get_track_route(track_id: UUID) -> PydanticModel:
+    """
+    Get track by id.
+    :param track_id: track id
+    :return: track
+    """
+    track: Track = await Track.get(id=track_id)
     response: PydanticModel = await TrackOut.from_tortoise_orm(track)
 
     return response
